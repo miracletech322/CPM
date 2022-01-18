4e619caa9774fbd0c51cd301666b71262c506daf
AliSherazi12 <nostreak1198@gmail.com>
2022-01-18 05:23:33 +0500

    na

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -7,8 +7,20 @@ use Illuminate\Http\Request;
 class WebhookController extends Controller
 {
     public function coinbase_webhooks(Request $request){
+
+        //Check if event->type = "charge:created" then do nothing
+
+        //if charge:confirmed,charge:delayed, charge:resolved find the public id of the user and compare the id and charge saved for the request and add to ledger/payment/update coinbase payment
+
+        //if charge:failed update coinbase payment , add to ledger
+
+        //if charge:pending -> only add to ledger
+
+        //ASK SUPPORT
+        //if charge:resolved, charge:delayed, charge:pending : Does the charge:confirmed webbook called after stated three
+
         info("Coin base webhook call");
-        info($request->all());
+        info(json_encode($request->event));
         return true;
     }
 }
