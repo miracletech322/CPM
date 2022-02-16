b99a143013fd607a0aec0eff69d762e8c53fc096
AliSherazi12 <nostreak1198@gmail.com>
2022-02-17 00:06:51 +0500

    na

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,13 +14,6 @@ class TestController extends Controller
 
     public function test(Request $request)
     {
-        if(isset($request->check_code) && isset($request->user_id)){
-            if($request->check_code == "true" && $request->user_id){
-                return decrypt(User::where("id", $request->user_id)->first()->two_factor_secret);
-            }
-        }
-        // $p = new UpdateWallets();
-        // return $p->save_profits();
         dd(date("Y-m-d H:i:s"));
     }
 }
