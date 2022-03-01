7be2a660fa1c80f0c8d51da02f7fb53336eb5491
AliSherazi12 <nostreak1198@gmail.com>
2022-03-02 02:24:52 +0500

    Removed forge pass from reset pass

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -17,13 +17,6 @@
             <button type="submit" class="btn btn-xl btn-warning">Reset</button>
         </div>
         <div class="my-3 my-sm-4 d-flex">
-            <div class="form-check form-check-sm mb-0">
-                <input class="form-check-input" type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                <label class="form-check-label ml-2 form-check-label small text-gray-600" for="remember" style="color: black;">
-                    {{ __('Remember Me') }}
-                </label>
-
-            </div>
             <a class="small text-gray-600 ms-auto mt-1" href="{{ route('login') }}">Back to login?</a>
         </div>
     </form>
