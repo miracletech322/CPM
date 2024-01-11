8b7dcaa5241ce4fa7c6a2a34b43dad9c3732a925
AliSherazi12 <nostreak1198@gmail.com>
2024-01-12 01:02:35 +0500

    na

--- resources/views/layouts/auth/footer.blade.php
+++ resources/views/layouts/auth/footer.blade.php
@@ -1,10 +1,8 @@
 <footer>
     <div class="row">
         <div class="col-lg-12 col-md-12 text-center">
-            <small>
-                <a href='{{url('lang/en')}}' class='text-primary {{session()->get('locale') == "en" ? "fw-bold" : ""}}'>{{__("English")}}</a> |
-                <a href='{{url('lang/de')}}' class='text-primary {{session()->get('locale') == "de" ? "fw-bold" : ""}}'>{{__("German")}}</a>
-            </small>
+            <a href='{{url('lang/en')}}' class='text-primary {{session()->get('locale') == "en" ? "fw-bold" : ""}}'>{{__("English")}}</a> |
+            <a href='{{url('lang/de')}}' class='text-primary {{session()->get('locale') == "de" ? "fw-bold" : ""}}'>{{__("German")}}</a>
         </div>
     </div>
 </footer>
